3217ee2f0a7bf293e47c799cd430d38c5780c930
sanjaysanju06 <sanjay.damera56@gmail.com>
2019-01-11 16:02:02 -0500

    Add files via upload

--- angularapp1/src/app/movie/listuser.component.ts
+++ angularapp1/src/app/movie/listuser.component.ts
@@ -2,17 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { Iuser } from './Iuser';
 import { UserService } from './user.service';
 import {  Router } from '@angular/router';
+import {PageEvent,MatFormFieldModule,MatInputModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormsModule ,FormBuilder, FormGroup} from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {   MatSortModule,MatTableDataSource} from "@angular/material";
+
+
+
+
+
+@NgModule({
+  imports: [
+      MatFormFieldModule,
+      MatInputModule,
+      FormsModule,
+      MatPaginatorModule,
+      PageEvent,
+      MatTableModule,
+      ReactiveFormsModule,
+      FormControl,
+      FormBuilder, 
+      FormGroup,
+      MatSortModule,
+      MatTableDataSource
+      ]
+})
 
 @Component({
   selector: 'app-listuser',
   templateUrl: './listuser.component.html',
   styleUrls: ['./listuser.component.css']
 })
+ 
 export class ListuserComponent implements OnInit {
  user:Iuser[];
+ lengths = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageEvent: PageEvent;
+  private allItems: any[];
+  pager: any = {};
+  pagedItems: any[];
   constructor(private _userservice: UserService,
-              private _router:Router,
-              ) { }
+              private _router:Router            ) { }
 
   ngOnInit() {
     this._userservice.getEmployees().subscribe(
@@ -22,10 +56,9 @@ export class ListuserComponent implements OnInit {
     ); 
    }
 
-   editButtonClick(userId:number){
-     this._router.navigate(['/loginedit',userId]);
-
-
-   }
-
+   
+   
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  }
 }
